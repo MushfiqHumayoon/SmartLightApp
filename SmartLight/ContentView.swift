bce45329e0b72d99dd17790cba664ea24ac9799b
Mushfiq Humayoon <mushfiqhumayoon@gmail.com>
2024-01-18 12:07:35 +0400

    Initial Commit

---
+++ SmartLight/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SmartLight
+//
+//  Created by Mushfiq Humayoon on 18/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SmartLight/SmartLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartLightApp.swift
+//  SmartLight
+//
+//  Created by Mushfiq Humayoon on 18/01/24.
+//
+
+import SwiftUI
+
+@main
+struct SmartLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
